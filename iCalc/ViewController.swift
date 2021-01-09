74350a74ad8d62fd7ebabbbfd6f6838518ba1b0f
Filip Sasinowski <filip.sasinowski@gmail.com>
2021-01-09 11:47:41 +0100

    Initial Commit

---
+++ iCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iCalc
+//
+//  Created by Filip Sasinowski on 09/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
