b0c222d00d24023e818415586050321821aae561
Filip Sasinowski <filip.sasinowski@gmail.com>
2021-01-09 13:03:12 +0100

    Created responsive storyboard layout.

--- iCalc/ViewController.swift
+++ iCalc/ViewController.swift
@@ -9,11 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Label that shows equations outcome
+    @IBOutlet weak var outcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        
+    }
+    @IBAction func oppositeButtonPressed(_ sender: UIButton) {
+        
+    }
+    @IBAction func percentButtonPressed(_ sender: UIButton) {
+        
+    }
+    @IBAction func divideButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func multiplyButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func minusButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func plusButtonPressed(_ sender: UIButton) {
+    }
+    @IBAction func equalsButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonNumberZeroPressed(_ sender: UIButton) {
+    }
+    @IBAction func buttonNumberOnePressed(_ sender: UIButton) {
+    }
+    @IBAction func buttonNumberTwoPressed(_ sender: UIButton) {
+    }
+    @IBAction func buttonNumberThreePressed(_ sender: UIButton) {
+    }
+    @IBAction func buttonNumberFourPressed(_ sender: Any) {
+    }
+    @IBAction func buttonNumberFivePressed(_ sender: UIButton) {
+    }
+    @IBAction func buttonNumberSixPressed(_ sender: UIButton) {
+    }
+    @IBAction func buttonNumberSevenPressed(_ sender: UIButton) {
+    }
+    @IBAction func buttonNumberEightPressed(_ sender: UIButton) {
+    }
+    @IBAction func buttonNumberNinePressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func commaButtonPressed(_ sender: UIButton) {
+    }
+    
+    
 }
 
